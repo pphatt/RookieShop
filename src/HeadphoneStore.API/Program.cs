f7626cc4648320cc46fb03548cd92245738d83ba
pphatt <vutienphat0809@gmail.com>
2025-04-11 21:40:44 +0700

    feat: setup controller layer

--- src/HeadphoneStore.API/Program.cs
+++ src/HeadphoneStore.API/Program.cs
@@ -1,3 +1,7 @@
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+using HeadphoneStore.API;
 using HeadphoneStore.API.DependencyInjection.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,6 +10,19 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Add Controller
+builder.Services
+    .AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+        options.JsonSerializerOptions.DictionaryKeyPolicy = JsonNamingPolicy.CamelCase;
+        options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter(JsonNamingPolicy.CamelCase));
+        options.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
+        options.JsonSerializerOptions.WriteIndented = true;
+    })
+    .AddApplicationPart(AssemblyReference.Assembly);
 
 // Add SeriLog
 builder.Host.AddLogging();
