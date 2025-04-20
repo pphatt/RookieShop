e1ce5e2b029b0dc932d43bc6e9a0033a014a78c2
pphatt <vutienphat0809@gmail.com>
2025-04-20 23:42:13 +0700

    feat: clean up components

---
+++ src/HeadphoneStore.StoreFrontEnd/Pages/Product/Product.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HeadphoneStore.StoreFrontEnd.Pages.Product
+{
+    public class ProductModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
