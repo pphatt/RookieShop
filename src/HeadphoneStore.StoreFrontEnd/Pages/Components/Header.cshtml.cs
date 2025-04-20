ca10c0b6dc3541f05786728e7c7006ede269eb6f
pphatt <vutienphat0809@gmail.com>
2025-04-20 22:07:33 +0700

    feat: add customer frontend home page

---
+++ src/HeadphoneStore.StoreFrontEnd/Pages/Components/Header.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HeadphoneStore.StoreFrontEnd.Pages.Components
+{
+    public class HeaderModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
