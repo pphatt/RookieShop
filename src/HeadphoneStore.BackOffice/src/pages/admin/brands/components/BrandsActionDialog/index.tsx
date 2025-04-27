3334137812a62e91bb72683b2f818978710452c5
pphatt <vutienphat0809@gmail.com>
2025-04-27 08:41:39 +0700

    feat: clean up error handling

--- src/HeadphoneStore.BackOffice/src/pages/admin/brands/components/BrandsActionDialog/index.tsx
+++ src/HeadphoneStore.BackOffice/src/pages/admin/brands/components/BrandsActionDialog/index.tsx
@@ -27,6 +27,7 @@ import { useMutation } from "@tanstack/react-query"
 import { AddNewBrand, UpdateBrand } from "@/services/brand.service"
 import { toast } from "react-toastify"
 import IconSpinner from "@/components/icon-spinner"
+import { handleError } from "@/utils"
 
 const schema = z.object({
   name: z.string().min(1, { message: "Last Name is required." }),
@@ -80,21 +81,7 @@ export function BrandsActionDialog({
             handleResetDialog()
           },
           onError: (error: any) => {
-            if (error?.response?.data?.title) {
-              let errorMessage = error.response.data.title
-
-              if (error?.response?.data?.description) {
-                errorMessage += ` - ${error.response.data.description}`
-              }
-
-              toast.error(errorMessage)
-            } else if (error?.response?.data?.Error?.Message) {
-              toast.error(error.response.data.Error.Message)
-            } else if (error?.response?.data?.message) {
-              toast.error(error.response.data.message)
-            } else {
-              toast.error("Something went wrong")
-            }
+            handleError(error)
           },
         }
       )
@@ -109,21 +96,7 @@ export function BrandsActionDialog({
         handleResetDialog()
       },
       onError: (error: any) => {
-        if (error?.response?.data?.title) {
-          let errorMessage = error.response.data.title
-
-          if (error?.response?.data?.description) {
-            errorMessage += ` - ${error.response.data.description}`
-          }
-
-          toast.error(errorMessage)
-        } else if (error?.response?.data?.Error?.Message) {
-          toast.error(error.response.data.Error.Message)
-        } else if (error?.response?.data?.message) {
-          toast.error(error.response.data.message)
-        } else {
-          toast.error("Something went wrong")
-        }
+        handleError(error)
       },
     })
   }
--- src/HeadphoneStore.BackOffice/src/utils/index.ts
+++ src/HeadphoneStore.BackOffice/src/utils/index.ts
@@ -1,7 +1,27 @@
+import { toast } from "react-toastify"
+
 export const toTitleCase = (str: string) => {
   if (!str) return str
 
   return str
     .toLowerCase()
     .replace(/(^|\s)\w/g, (letter) => letter.toUpperCase())
+}
+
+export const handleError = (error: any) => {
+  if (error?.response?.data?.title) {
+    let errorMessage = error.response.data.title
+
+    if (error?.response?.data?.description) {
+      errorMessage += ` - ${error.response.data.description}`
+    }
+
+    toast.error(errorMessage)
+  } else if (error?.response?.data?.Error?.Message) {
+    toast.error(error.response.data.Error.Message)
+  } else if (error?.response?.data?.message) {
+    toast.error(error.response.data.message)
+  } else {
+    toast.error("Something went wrong")
+  }
 }
