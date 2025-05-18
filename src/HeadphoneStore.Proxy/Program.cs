8dfb9478bc46a6a319f87fdc6b3d69ec15d47e67
pphatt <vutienphat0809@gmail.com>
2025-05-18 09:06:01 +0700

    feat: add rate limit for api gateway

--- src/HeadphoneStore.Proxy/Program.cs
+++ src/HeadphoneStore.Proxy/Program.cs
@@ -1,3 +1,7 @@
+using System.Threading.RateLimiting;
+
+using Microsoft.AspNetCore.RateLimiting;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.AddServiceDefaults();
@@ -11,9 +15,24 @@ builder.Services.AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"))
     .AddServiceDiscoveryDestinationResolver();
 
+// YARP - Configure RateLimiter Service to prevent DoS attacks
+builder.Services.AddRateLimiter(options =>
+{
+    options.AddFixedWindowLimiter("rateLimitPolicy", opt =>
+    {
+        opt.PermitLimit = 10;
+        opt.Window = TimeSpan.FromSeconds(15);
+        opt.QueueProcessingOrder = QueueProcessingOrder.OldestFirst;
+        opt.QueueLimit = 2;
+    });
+});
+
 var app = builder.Build();
 
 app.MapDefaultEndpoints();
+
+app.UseRateLimiter();
+
 app.MapReverseProxy();
 
 await app.RunAsync();
